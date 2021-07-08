51fbabf9989bec4bc35016c3618f9fe2d4c3d621
linchuanghu <linchuanghu@addcn.com>
2021-07-08 09:27:19 +0800

    Clear project

--- src/com/lake/json2dart/generator/Exceptions.kt
+++
@@ -1,9 +0,0 @@
-package com.lake.json2dart.generator
-
-import java.io.IOException
-
-class SyntaxException : Exception("Wrong json syntax")
-
-class FileIOException : IOException("Cannot read or write file")
-
-class NotFlutterProject : Exception("It does not look like a flutter project, and cannot find 'lib' folder.")
